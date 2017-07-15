66177b6afdfa593df81df9d096193c5f57907fc7
idzhalalov <idzhalalov@gmail.com>
2017-07-16 00:14:12 +0500

    В контроллер внедрен шаблонизатор и новые атрибуты (представление и модель)

--- lib/core/Controller.php
+++ lib/core/Controller.php
@@ -1,12 +1,26 @@
 <?php
 namespace TestApp\Lib;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
 abstract class Controller
 {
     protected $app;
+    protected $view;
+    protected $model;
+
     public function __construct(Application $app)
     {
         $this->app = $app;
+
+        // model
+        $this->model = $this->app->getModel();
+
+        // views
+        $loader = new Twig_Loader_Filesystem($this->app->config['path']['views']);
+        $this->view = new Twig_Environment($loader, array(
+            'cache' => $this->app->config['path']['views_cache'],
+        ));
     }
 
     public function post($var, $default = null)
