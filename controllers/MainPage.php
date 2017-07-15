27efd399e0134ca5ce08b848db0a86943f715c08
idzhalalov <idzhalalov@gmail.com>
2017-07-15 13:45:56 +0500

    Базовый контроллер теперь умеет получать переменные из GET и POST

---
+++ controllers/MainPage.php
@@ -0,0 +1,12 @@
+<?php
+namespace TestApp\Controllers;
+
+use TestApp\Lib\Controller;
+
+class MainPage extends Controller
+{
+    public function index()
+    {
+        echo 'Hello';
+    }
+}
--- lib/Controller.php
+++ lib/Controller.php
@@ -1,14 +1,23 @@
 <?php
+namespace TestApp\Lib;
 
-/**
- * Created by IntelliJ IDEA.
- * User: idzhalalov
- * Date: 15.07.17
- * Time: 11:38 AM
- */
-namespace Lib\Controller;
-
-class Controller
+abstract class Controller
 {
+    public function post($var, $default = null)
+    {
+        if (isset($_POST[$var])) {
+            return $_POST[$var];
+        } else {
+            return $default;
+        }
+    }
 
+    public function get($var, $default = null)
+    {
+        if (isset($_GET[$var])) {
+            return $_GET[$var];
+        } else {
+            return $default;
+        }
+    }
 }
