fb70cb2548b6226fc0befbc1342ff00610a9451d
idzhalalov <idzhalalov@gmail.com>
2017-07-15 13:27:30 +0500

    Добавлена автозагрузка библиотек и функция выполнения контроллера с нужым методом и аргументами

--- index.php
+++ index.php
@@ -6,15 +6,37 @@
  * Time: 11:19 AM
  */
 require __DIR__ . '/vendor/autoload.php';
+spl_autoload_register(function () {
+    include __DIR__ . '/lib/Controller.php';
+    include __DIR__ . '/lib/Model.php';
+    include __DIR__ . '/lib/View.php';
+});
+
+function controllerExec($controller, $method, array $args = [])
+{
+    $classFilename = __DIR__ . "/controllers/$controller.php";
+    spl_autoload_register(function () use ($classFilename) {
+        if (file_exists($classFilename)) {
+            include $classFilename;
+        } else {
+            throw new InvalidArgumentException("Class $classFilename did not found");
+        }
+    });
+    $className = '\TestApp\Controllers\\' . $controller;
+    if (class_exists($className)) {
+        $class = new $className();
+    }
+    if (method_exists($className, $method)) {
+        call_user_func_array([$class, $method], $args);
+    }
+}
 
 // Create Router instance
 $router = new \Bramus\Router\Router();
-$controller = new \Lib\Controller\Controller();
+
 // Routes
-$router->match('GET', '/', function() {
-    echo 'main page';
-});
-$router->match('GET', '/ololo/', function() {
+$router->get('/', controllerExec('MainPage', 'index'));
+$router->match('GET', '/ololo/', function () {
     echo 'not main page';
 });
 $router->run();
