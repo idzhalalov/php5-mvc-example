6f897d1d9c50038f4706d08301503861b8e9042a
idzhalalov <idzhalalov@gmail.com>
2017-07-15 13:49:20 +0500

    Контроллер админа

---
+++ controllers/AdminPage.php
@@ -0,0 +1,12 @@
+<?php
+namespace TestApp\Controllers;
+
+use TestApp\Lib\Controller;
+
+class AdminPage extends Controller
+{
+    public function index()
+    {
+        echo 'Hello, Admin';
+    }
+}
