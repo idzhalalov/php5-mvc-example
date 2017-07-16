fb07d41d50a8a6d449f368d734f0012d3b412a49
idzhalalov <idzhalalov@gmail.com>
2017-07-16 18:09:04 +0500

    Добавлена авторизация и шаблон админки

--- lib/controllers/AdminPage.php
+++ lib/controllers/AdminPage.php
@@ -7,6 +7,25 @@ class AdminPage extends Controller
 {
     public function index()
     {
-        echo 'Hello Admin';
+        if (!isset($_SESSION['admin'])) {
+            $this->app->ApplicationError('You must login first');
+        }
+        $this->view->display('template_admin.twig');
+    }
+
+    public function login()
+    {
+        $username = $this->post('username');
+        $password = $this->post('password');
+
+        if ($username === null || $password === null) {
+            $this->app->ApplicationError('Please, provide correct Username and Password');
+        }
+
+        if ($username === 'admin' && $password === '123') {
+            $_SESSION['admin'] = 1;
+            header("Location: {$_SERVER['HTTP_ORIGIN']}/admin");
+            exit();
+        }
     }
 }
