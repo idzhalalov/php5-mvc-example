3845232e6fa302a128a07fb3ea59b7d338d66417
idzhalalov <idzhalalov@gmail.com>
2017-07-16 16:24:00 +0500

    пагинация для задач

--- lib/controllers/MainPage.php
+++ lib/controllers/MainPage.php
@@ -1,22 +1,53 @@
 <?php
 namespace TestApp\Controllers;
 
-use TestApp\Lib\Controller;
 use TestApp\Lib\Application;
+use TestApp\Lib\Controller;
 
 class MainPage extends Controller
 {
     private $model;
+    private $recordsPerPage;
 
     public function __construct(Application $app)
     {
         parent::__construct($app);
         $this->model = $app->getModel('Tasks');
+        $this->recordsPerPage = 3;
     }
 
     public function index()
     {
-        $data = $this->model->get([], 3);
-        $this->view->display('template.twig', ['tasks' => $data]);
+        // pagination
+        $rowsCount = $this->model->rowsCount();
+        $pagesCount = ceil($rowsCount / $this->recordsPerPage);
+
+        $data = $this->model->get([], $this->recordsPerPage);
+        $this->view->display('template.twig', [
+            'tasks' => $data,
+            'pagesCount' => $pagesCount
+        ]);
+    }
+
+    public function tasks($pageNum)
+    {
+        // pagination
+        $rowsCount = $this->model->rowsCount();
+        $pagesCount = ceil($rowsCount / $this->recordsPerPage);
+
+        // current, prev, next page
+        $endRecord = $this->recordsPerPage * $pageNum;
+        $startRecord = $endRecord - $this->recordsPerPage;
+        $prevPage = ($pageNum > 1) ? $pageNum - 1 : 1;
+        $nextPage = ($pageNum < $pagesCount) ? $pageNum + 1 : $pagesCount;
+        $data = $this->model->get([], "$startRecord, {$this->recordsPerPage}");
+
+        $this->view->display('template.twig', [
+            'tasks' => $data,
+            'pagesCount' => $pagesCount,
+            'currentPage' => $pageNum,
+            'prevPage' => $prevPage,
+            'nextPage' => $nextPage,
+        ]);
     }
 }
