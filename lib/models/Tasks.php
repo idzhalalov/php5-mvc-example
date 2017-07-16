6260472df7e209fedb6e4322ed9a1ae9ac00f1a2
idzhalalov <idzhalalov@gmail.com>
2017-07-16 15:49:40 +0500

    Добавлен метод для подсчета кол-ва строк

--- lib/models/Tasks.php
+++ lib/models/Tasks.php
@@ -5,5 +5,9 @@ use TestApp\Lib;
 
 class Tasks extends Lib\Model
 {
-
+    public function rowsCount()
+    {
+        $sql = 'SELECT count(id) as rows FROM ' . $this->tableName;
+        return $this->pdo->query($sql)->fetch()['rows'];
+    }
 }
