013302f268244987a6955776a771b9888816f230
idzhalalov <idzhalalov@gmail.com>
2017-07-17 23:58:24 +0500

    Добавлена функция сохранения задачи

--- lib/models/Tasks.php
+++ lib/models/Tasks.php
@@ -1,6 +1,7 @@
 <?php
 namespace TestApp\Models;
 
+use Psr\Log\InvalidArgumentException;
 use TestApp\Lib;
 
 class Tasks extends Lib\Model
@@ -9,5 +10,31 @@ class Tasks extends Lib\Model
     {
         $sql = 'SELECT count(id) as rows FROM ' . $this->tableName;
         return $this->pdo->query($sql)->fetch()['rows'];
+    }
+
+    /**
+     * Saves task
+     *
+     * @param array $data
+     * @param null $id
+     *
+     * @return int Id of brand new task
+     * @throws InvalidArgumentException
+     */
+    public function saveTask(array $data, $id = null)
+    {
+        if (!$data) {
+            throw new InvalidArgumentException(__METHOD__ . ' requires non empty array "$data"');
+        }
+        $dataValues = array_values($data);
+        $dataFields = implode(', ', array_keys($data));
+        $placeholders = str_repeat('?, ', count($dataValues));
+        $placeholders = substr($placeholders, 0, strlen($placeholders) - 2);
+        if ($id === null) {
+            $sql = "INSERT INTO $this->tableName ($dataFields) VALUES ($placeholders)";
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute($dataValues);
+        }
+        return $this->pdo->lastInsertId();
     }
 }
