778f698942995e72a96c7ce3868773076da45ff3
idzhalalov <idzhalalov@gmail.com>
2017-07-18 23:09:30 +0500

    Метод сохранения теперь умеет и создавать и сохранять

--- lib/models/Tasks.php
+++ lib/models/Tasks.php
@@ -16,22 +16,31 @@ class Tasks extends Lib\Model
      * Saves task
      *
      * @param array $data
-     * @param null $id
+     * @param int $id
      *
      * @return int Id of brand new task
      * @throws InvalidArgumentException
      */
-    public function saveTask(array $data, $id = null)
+    public function saveTask(array $data, $id = 0)
     {
+        $id = (int) $id;
         if (!$data) {
             throw new InvalidArgumentException(__METHOD__ . ' requires non empty array "$data"');
         }
         $dataValues = array_values($data);
-        $dataFields = implode(', ', array_keys($data));
-        $placeholders = str_repeat('?, ', count($dataValues));
-        $placeholders = substr($placeholders, 0, strlen($placeholders) - 2);
-        if ($id === null) {
+        if ($id === 0) {
+            $dataFields = implode(', ', array_keys($data));
+            $placeholders = str_repeat('?, ', count($dataValues));
+            $placeholders = substr($placeholders, 0, strlen($placeholders) - 2);
+
             $sql = "INSERT INTO $this->tableName ($dataFields) VALUES ($placeholders)";
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute($dataValues);
+        } else {
+            $dataFields = implode(' = ?, ', array_keys($data)) . ' = ?';
+            $dataValues[] = $id;
+
+            $sql = "UPDATE $this->tableName SET $dataFields WHERE id = ?";
             $statement = $this->pdo->prepare($sql);
             $statement->execute($dataValues);
         }
